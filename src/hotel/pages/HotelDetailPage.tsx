0881729ff84f63b2779c29d5622fb0f68052ea8a
Juan P. Isaza <jisazamp@gmail.com>
2022-09-27 15:01:24 -0500

    Removed unused dependencies

--- src/hotel/pages/HotelDetailPage.tsx
+++ src/hotel/pages/HotelDetailPage.tsx
@@ -1,8 +1,4 @@
 import HotelLayout from '../layout/HotelLayout'
-import { useParams } from 'react-router-dom'
-import { useAppSelector } from '../../store'
-
-import NotFound from '../../ui/components/NotFound'
 
 const HotelDetailPage = () => {
   return (
